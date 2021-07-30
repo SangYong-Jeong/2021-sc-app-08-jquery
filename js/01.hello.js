422e7ff201b0c36f8e6c951378a70e2ccf0c1322
SangYong-Jeong <mmaf@naver.com>
2021-07-30 15:02:02 +0900

    5교시 끝

--- js/01.hello.js
+++ js/01.hello.js
@@ -1,4 +1,8 @@
-var jQuery = function(el) {
-	return document.querySelectorAll(el);
-}
+var img = document.getElementById('img');
+var $img = $('#img');
+
+console.log(img.src);
+console.log($(img).attr('src'));
+console.log($(img)[0].src);
+
 
---
+++ js/02.bulb.js
@@ -0,0 +1,25 @@
+/* $('.bt-on').on('click', function() {
+	console.log('ON');
+}); */
+
+
+
+/* document.getElementsByClassName('bt-on')[0].addEventListener('click', function() {
+	console.log('ON');
+})
+document.getElementsByClassName('bt-on')[1].addEventListener('click', function() {
+	console.log('ON');
+})
+document.getElementsByClassName('bt-on')[2].addEventListener('click', function() {
+	console.log('ON');
+}) */
+
+// ES5/ES6/jQuery 선택자
+// document.getElementById('idName') -> html
+// document.getElementsByClassName('className') -> Array[html, html]
+// document.getElementsByTagName('tagName') -> Array[html, html]
+
+// document.querySelector('.w100') -> html
+// document.querySelectorAll('.w100') -> Array[html, html]
+
+// $('.w100') -> jQuery
