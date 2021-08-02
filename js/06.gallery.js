13be96904e6e35cecec5cb35fde02ffa76a3ec2a
SangYong-Jeong <mmaf@naver.com>
2021-08-02 13:20:26 +0900

    4교시 끝

---
+++ js/06.gallery.js
@@ -0,0 +1,9 @@
+var foods = [
+	{name:'Popcicle', src:'../img/f1.jpg'},
+	{name:'Salmon', src:'../img/f2.jpg'},
+	{name:'Sandwitch', src:'../img/f3.jpg'},
+	{name:'Steak', src:'../img/f4.jpg'},
+	{name:'Wine', src:'../img/f5.jpg'},
+	{name:'Cherry', src:'../img/f6.jpg'},
+	{name:'Croissant', src:'../img/f7.jpg'}
+];
