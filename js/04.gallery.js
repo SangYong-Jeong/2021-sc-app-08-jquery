9d52a77d0dd82deea79d9e3cdd97c6bc22f66b33
SangYong-Jeong <mmaf@naver.com>
2021-08-02 10:25:48 +0900

    1교시 끝

---
+++ js/04.gallery.js
@@ -0,0 +1,72 @@
+// AJAX (Asyncronous Javascript And XML)
+var products = [
+	{
+		name: 'Good Product1',
+		src: '../img/p1.jpg',
+		price: 12.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product2',
+		src: '../img/p2.jpg',
+		price: 13.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product3',
+		src: '../img/p3.jpg',
+		price: 13.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product4',
+		src: '../img/p4.jpg',
+		price: 14.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product5',
+		src: '../img/p5.jpg',
+		price: 15.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product6',
+		src: '../img/p6.jpg',
+		price: 16.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product7',
+		src: '../img/p7.jpg',
+		price: 17.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product8',
+		src: '../img/p8.jpg',
+		price: 18.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product9',
+		src: '../img/p9.jpg',
+		price: 19.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product10',
+		src: '../img/p10.jpg',
+		price: 120.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product11',
+		src: '../img/p11.jpg',
+		price: 121.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product12',
+		src: '../img/p12.jpg',
+		price: 122.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, 
+]
+
+
+// JAVA - Jakson/Gson
+console.log(JSON.stringify(products));
+var json = JSON.stringify(products); // js Object -> json
+var jsObj = JSON.parse(json); // json -> js Object
+console.log(json);
+console.log(jsObj);
