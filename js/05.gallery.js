d8875b2d8c3fdb51f8348e2cf4b0483b8976d2ef
SangYong-Jeong <mmaf@naver.com>
2021-08-02 12:16:54 +0900

    3교시 끝

--- js/05.gallery.js
+++ js/05.gallery.js
@@ -1,3 +1,5 @@
+// window, Math, String, Number, Date, Array, Console, JSON, Object
+
 var products = [
 	{
 		name: 'Good Product1',
@@ -62,22 +64,12 @@ var products = [
 	}, 
 ]
 
-
-// JAVA - Jakson/Gson
-console.log(JSON.stringify(products));
-var json = JSON.stringify(products); // js Object -> json
-var jsObj = JSON.parse(json); // json -> js Object
-// console.log(json);
-// console.log(jsObj);
-
-(function init() {
-	var cnt = products.length; // 12
-	var html = '';
-	var wrap = document.getElementsByClassName('prd-wrap')[0];
-	for(var i=0; i<cnt; i++) {
-		html = '<li class="prd">';
+$(document).ready(function() {
+	var cnt = products.length;
+	for(var i=0, html;i<cnt;i++) {
+		html  = '<li class="prd">';
 		html += '<div class="imgs">';
-		html += '<img src="'+products[i].src+'" class="w100" onclick="openModal('+i+');">';
+		html += '<img src="'+products[i].src+'" class="w100">';
 		html += '</div>';
 		html += '<div class="conts">';
 		html += '<h2 class="name">'+products[i].name+'</h2>';
@@ -85,21 +77,8 @@ var jsObj = JSON.parse(json); // json -> js Object
 		html += '<div class="price">$'+products[i].price+'</div>';
 		html += '</div>';
 		html += '</li>';
-		wrap.innerHTML += html;
+		// $('.prd-wrap').html( $('.prd-wrap').html() + html );
+		// $('.prd-wrap').prepend(html);
+		$('.prd-wrap').append(html);
 	}
-
-})(); // IIFE(즉시실행함수)
-
-// console.log(i);
-
-function openModal(id) {
-	var modal = document.getElementsByClassName('modal-wrapper')[0];
-	var img = modal.getElementsByTagName('img')[0];
-	img.src = products[id].src;
-	modal.style.display = 'flex'; 
-}
-
-function closeModal() {
-	var modal = document.getElementsByClassName('modal-wrapper')[0];
-	modal.style.display = 'none';
-}
+});
